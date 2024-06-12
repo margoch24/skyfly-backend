f29fa16265d01058c439e284395be9c88c868089
Margo <vitarita095788@gmail.com>
2024-06-12 22:08:42 +0300

    edit contact us model

--- api/models/contact_us.py
+++ api/models/contact_us.py
@@ -6,7 +6,7 @@ class ContactUs(BaseModel):
     __tablename__ = "contact_us"
 
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     name = db.Column(db.String(120))
     phone_number = db.Column(db.String(120))
     message = db.Column(db.Text, nullable=False)
